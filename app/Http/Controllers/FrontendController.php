39678d28cc435a325a88a167db047c7036c11af4
Likhon Barai <likhonhere007@gmail.com>
2024-12-23 12:45:24 +0600

    patient & doctor pages uploaded with routes

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -10,4 +10,29 @@ class FrontendController extends Controller
   public function showHomePage() {
     return view('frontend.home');
   }
+
+  // show Login Page
+  public function showLoginPage() {
+    return view('frontend.login');
+  }
+
+  // show Patient Register Page
+  public function showPatientRegisterPage() {
+    return view('frontend.patient.register');
+  }
+
+  // show Patient Dashboard Page
+  public function showPatientDashPage() {
+    return view('frontend.patient.dashboard');
+  }
+
+  // show Doctor Register Page
+  public function showDoctorRegisterPage() {
+    return view('frontend.doctor.register');
+  }
+
+  // show Doctor Dashboard Page
+  public function showDoctorDashPage() {
+    return view('frontend.doctor.dashboard');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use App\Http\Controllers\FrontendController;
 
 // Frontend Controller
 Route::get('/', [ FrontendController::class, 'showHomePage' ]) -> name('home.page');
+Route::get('/login', [ FrontendController::class, 'showLoginPage' ]) -> name('login.page');
+
+// Patient Pages
+Route::get('/patient-register', [ FrontendController::class, 'showPatientRegisterPage' ]) -> name('patient.reg.page');
+Route::get('/patient-dashboard', [ FrontendController::class, 'showPatientDashPage' ]) -> name('patient.desh.page');
+
+// Doctor Pages
+Route::get('/doctor-register', [ FrontendController::class, 'showDoctorRegisterPage' ]) -> name('doctor.reg.page');
+Route::get('/doctor-dashboard', [ FrontendController::class, 'showDoctorDashPage' ]) -> name('doctor.desh.page');
